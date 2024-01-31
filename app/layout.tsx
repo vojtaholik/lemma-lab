5a640383f6cea8b789e0510a809ecd97f26b8c23
Vojta Holik <vojta@egghead.io>
2024-01-31 12:30:55 +0100

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
+import { getAllPosts } from '@/lib/posts'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,15 +11,37 @@ export const metadata: Metadata = {
   description: 'Full of cool stuff!',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const posts = await getAllPosts()
+
   return (
     <html lang="en" className={`${inter.className} antialiased dark`}>
       <body className="text-gray-200 max-w-2xl m-auto">
-        <main className="p-6 pt-3 md:pt-6 min-h-screen">{children}</main>
+        <main className="p-6 pt-3 md:pt-6 min-h-screen flex gap-10 w-full max-w-screen-xl mx-auto">
+          <aside className="flex flex-col flex-shrink-0">
+            <Link href="/">Posts</Link>
+            <ul>
+              {posts.map(({ id, title }) => {
+                return (
+                  <li key={id}>
+                    <Link
+                      className={`border-b text-gray-600 border-gray-300 transition-[border-color] hover:border-gray-600 dark:text-white dark:border-gray-500 dark:hover:border-white`}
+                      href="/posts/[id]"
+                      as={`/posts/${id}`}
+                    >
+                      {title}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </aside>
+          {children}
+        </main>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,5 @@
 import { getAllPosts } from '@/lib/posts'
-import { H1 } from './posts/components/h1'
-import Link from 'next/link'
 
 export default async function Home() {
-  const posts = await getAllPosts()
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full max-w-screen-sm mx-auto">
-        <H1>Posts</H1>
-        <ul>
-          {posts.map(({ id, title }) => {
-            return (
-              <li key={id}>
-                <Link
-                  className={`border-b text-gray-600 border-gray-300 transition-[border-color] hover:border-gray-600 dark:text-white dark:border-gray-500 dark:hover:border-white`}
-                  href="/posts/[id]"
-                  as={`/posts/${id}`}
-                >
-                  {title}
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    </main>
-  )
+  return <div className="w-full">yo</div>
 }
